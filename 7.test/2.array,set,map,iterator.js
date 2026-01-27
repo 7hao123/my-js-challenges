6b7cb199795968e5fb5aa282b46252c0bdd32eae
qinhao <82450473@qq.com>
2026-01-27 11:09:04 +0800

    2026/1/27

---
+++ 7.test/2.array,set,map,iterator.js
@@ -0,0 +1,41 @@
+const map = new Map();
+map.set(1, 2);
+map.set(3, 4);
+map.set(5, 6);
+map.set(7, 8);
+// for (let item of map) {
+//   console.log(item);
+// }
+const set = new Set();
+set.add(1);
+set.add(3);
+set.add(5);
+set.add(7);
+for (let item of set) {
+  console.log(item);
+}
+for (let item of map.entries()) {
+  console.log(item);
+}
+const arr = Array.from(set);
+console.log(arr, "set");
+const arr2 = Array.from(map);
+console.log(arr2, "map");
+
+const obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+for (let item in obj) {
+  console.log(item);
+}
+for (let item of Object.keys(obj)) {
+  console.log(item);
+}
+for (let item of Object.values(obj)) {
+  console.log(item);
+}
+for (let item of Object.entries(obj)) {
+  console.log(item);
+}
